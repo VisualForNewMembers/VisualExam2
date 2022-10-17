b3b3483ab659dd551db6537f0e231d3652822577
叶逸俊 <3544991733@qq.com>
2022-10-17 23:13:32 +0800

    Add files via upload

---
+++ 作业二/作业二/作业二.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	char a[50];
+	int i, j;
+	int k=0;
+	scanf("%s", &a);
+	j = int(strlen(a));
+	for (i = 0; i < j + 1; i++)
+	{
+		if (a[i] >= '0' && a[i] <= '9')
+			continue;
+		else if (a[i] >= '<' && a[i] <= '>')
+			k = i;
+		else if(i==k+1 && a[i] =='=')
+			continue;
+		else if (a[i] == ';' && a[i + 1] == '\0')
+		{
+			printf("语法正确");
+			break;
+		}
+		else
+		{
+			printf("语法错误，在第%d列", i + 1);
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
