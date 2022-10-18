9ea14d699cfb8beb3442ed0b5a5d2c94acbd5d87
1773273494 <114221910+1773273494@users.noreply.github.com>
2022-10-18 20:23:10 +0800

    Update 作业二.cpp

--- 作业二/作业二/作业二.cpp
+++ 作业二/作业二/作业二.cpp
@@ -12,16 +12,26 @@ int main()
 	j = int(strlen(a));
 	for (i = 0; i < j + 1; i++)
 	{
-		if (a[i] >= '0' && a[i] <= '9')
+                if(i==0 && a[i]>='<' && a[i]<='>')
+                  {
+                        print("语法错误，第一个不是数字");
+                         break;
+                   }
+		else if (a[i] >= '0' && a[i] <= '9')
 			continue;
 		else if (l==0 &&  a[i] >= '<' && a[i] <= '>')
 		{
 			k = i;
 			l++;
 		}
+                 else if(l==1 && i==k+1 && a[i]==';')
+                   {
+                        printf("语法错误，倒数第二个不是数字");
+                         break;
+                    }
 		else if(l==1 && i==k+1 && a[i] =='=')
 			continue;
-		else if (a[i] == ';' && a[i + 1] == '\0')
+		else if (a[i-1]>='0' && a[i]<='9' && a[i] == ';' && a[i + 1] == '\0')
 		{
 			printf("语法正确");
 			break;
